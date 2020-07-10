b6c6bba83035e20ba2da571b5360902265c325cf
kaushikyelne <kaushik.s.yelne@gmail.com>
2020-07-10 15:58:35 +0530

    Add github pages config

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffectgit} from 'react';
 import {NativeSelect, FormControl} from '@material-ui/core';
 
 import styles from './CountryPicker.module.css';
